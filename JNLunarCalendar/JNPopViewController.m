971b6c2d96635505dcc4d763eca1596c976968c6
yuchenghai <yuchenghai@yixin.im>
2017-01-16 14:24:44 +0800

    年月的选择已经ok

--- JNLunarCalendar/JNPopViewController.m
+++ JNLunarCalendar/JNPopViewController.m
@@ -16,11 +16,18 @@
 @property (weak) IBOutlet NSView *headView;
 @property (weak) IBOutlet NSCollectionView *collectionView;
 @property (strong) JNCollectionItem *collectionItem;
+
 @property (weak) IBOutlet NSScrollView *yearScrollView;
 @property (weak) IBOutlet NSButton *yearButton;
 @property (weak) IBOutlet NSTableView *yearTableView;
 @property (strong) NSMutableArray *yearDataSouce;
-@property (assign) NSInteger yearSelectIndex;
+
+@property (weak) IBOutlet NSScrollView *monthScrollView;
+@property (weak) IBOutlet NSButton *monthButton;
+@property (weak) IBOutlet NSTableView *monthTableView;
+
+@property (assign) int currentYear;
+@property (assign) int currentMonth;
 
 @end
 
@@ -49,12 +56,6 @@
     [self.headView.layer setBackgroundColor:[[NSColor whiteColor] CGColor]];
     
     
-    
-    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-    [dateFormatter setDateFormat:@"yyyy"];
-    NSString *currentYear = [dateFormatter stringFromDate:[NSDate date]];
-    [_yearButton setTitle:currentYear];
-    
     self.collectionItem = [JNCollectionItem new];
     [_collectionView setItemPrototype:self.collectionItem];
     _collectionView.selectable = YES;
@@ -64,7 +65,12 @@
     [self.yearTableView setRowHeight:20];
     [self.yearTableView setHeaderView:nil];
     
-    [self reloadDataWithDate:[currentYear intValue] month:1];
+    [self.monthTableView setGridStyleMask:(NSTableViewSolidHorizontalGridLineMask | NSTableViewSolidVerticalGridLineMask)];
+    [self.monthTableView setRowHeight:20];
+    [self.monthTableView setHeaderView:nil];
+    
+    // 首次点位到今天
+    [self backToToDayClick:nil];
 }
 
 - (IBAction)quitClick:(id)sender {
@@ -102,6 +108,9 @@
 }
 
 - (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
+    if (self.monthTableView == tableView) {
+        return 12;
+    }
     return self.yearDataSouce.count;
 }
 
@@ -112,42 +121,112 @@
 }
 
 - (void)tableView:(NSTableView *)tableView didAddRowView:(NSTableRowView *)rowView forRow:(NSInteger)row {
-    NSTextField *textField = [rowView viewWithTag:31];
-    NSString *title = [self.yearDataSouce objectAtIndex:row];
-    [textField setStringValue:title];
-    
-    if ([_yearButton.title isEqualToString:title]) {
-        [rowView setSelected:YES];
-        _yearSelectIndex = row;
+    if (self.monthTableView == tableView) {
+        NSTextField *textField = [rowView viewWithTag:31];
+        [textField setStringValue:[NSString stringWithFormat:@"%zd 月", row+1]];
+        
+    } else {
+        NSTextField *textField = [rowView viewWithTag:31];
+        NSString *title = [self.yearDataSouce objectAtIndex:row];
+        [textField setStringValue:title];
+        
+        if ([title intValue] == _currentYear) {
+            [rowView setSelected:YES];
+        }
     }
 }
 
--(void)tableViewSelectionDidChange:(NSNotification *)notification{
+-(void)tableViewSelectionDidChange:(NSNotification *)notification {
     if ([notification object] == self.yearTableView) {
-        _yearSelectIndex = [[notification object] selectedRow];
-        NSString *year = [self.yearDataSouce objectAtIndex:_yearSelectIndex];
-        [_yearButton setTitle:year];
+        NSString *year = [self.yearDataSouce objectAtIndex:[[notification object] selectedRow]];
         [self.yearScrollView setHidden:YES];
         // 刷新日历
-        [self reloadDataWithDate:[year intValue] month:1];
+        _currentYear = [year intValue];
+        [self reloadDateData];
     }
 }
 
 - (IBAction)showYearPopUpMenu:(id)sender {
-//    [self.yearScrollView setHidden:!self.yearScrollView.isHidden];
     if (self.yearScrollView.hidden) {
         [self.yearScrollView setHidden:NO];
         // 滚动过去
-        [self.yearScrollView.documentView scrollPoint:NSMakePoint(0, _yearSelectIndex*20)];
+        int yearSelectIndex = _currentYear - 1990;
+        [self.yearScrollView.documentView scrollPoint:NSMakePoint(0, yearSelectIndex*20)];
     } else {
         [self.yearScrollView setHidden:YES];
     }
+}
+
+- (IBAction)showMonthPopUpMenu:(id)sender {
+    if (self.monthScrollView.hidden) {
+        [self.monthScrollView setHidden:NO];
+    } else {
+        [self.monthScrollView setHidden:YES];
+    }
+}
+
+- (IBAction)nextMonthButtonClick:(id)sender {
+    if (_currentMonth+1 > 12) {
+        _currentMonth = 1;
+        _currentYear ++;
+        
+        // 超出范围
+        if (_currentYear > 2050) {
+            _currentMonth = 12;
+            _currentYear --;
+            return;
+        }
+    } else {
+        _currentMonth ++;
+    }
+    [self reloadDateData];
+}
+
+- (IBAction)backToToDayClick:(id)sender {
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy"];
+    NSString *currentYear = [dateFormatter stringFromDate:[NSDate date]];
+    [dateFormatter setDateFormat:@"MM"];
+    NSString *currentMonth = [dateFormatter stringFromDate:[NSDate date]];
+    
+    _currentYear = [currentYear intValue];
+    _currentMonth = [currentMonth intValue];
+    [self reloadDateData];
+    
+}
+
+- (IBAction)preMonthButtonClick:(id)sender {
+    if (_currentMonth-1 < 1) {
+        _currentMonth = 12;
+        _currentYear --;
+        // 超出范围
+        if (_currentYear < 1990) {
+            _currentMonth = 1;
+            _currentYear ++;
+            return;
+        }
+    } else {
+        _currentMonth --;
+    }
+    [self reloadDateData];
 }
 
 - (void)viewDidDisappear {
     [super viewDidDisappear];
     
     [self.yearScrollView setHidden:YES];
+}
+
+- (void)reloadDateData {
+    [self reloadDataWithDate:_currentYear month:_currentMonth];
+    
+    [_yearButton setTitle:[NSString stringWithFormat:@"%zd 年", _currentYear]];
+    [_monthButton setTitle:[NSString stringWithFormat:@"%zd 月", _currentMonth]];
+    
+    [self.monthScrollView setHidden:YES];
+    [self.yearScrollView setHidden:YES];
+    [self.yearTableView reloadData];
 }
 
 
